34a776a7e3e18127ac46532bd91bab95f466a46c
tao <xue250run@163.com>
2019-08-04 17:46:20 +0800

    init project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,96 @@
+module.exports = {
+    title: "Tom xu's blog",
+    description:
+        '个人知识框架整理，便于查缺漏。前端 HTML CSS ES JS Webpack Test Git Vue React Node Express Koa',
+    themeConfig: {
+        lastUpdated: '更新时间：',
+        nav: [
+            { text: 'HTML', link: '/HTML/' },
+            { text: 'CSS', link: '/CSS/' },
+            { text: 'ES', link: '/ES/' },
+            { text: 'Browser', link: '/Browser/' },
+            {
+                text: 'FE-Framework',
+                items: [
+                    {
+                        text: 'Web',
+                        items: [
+                            { text: 'Vue', link: '/FE-Framework/Vue/' },
+                            { text: 'React', link: '/FE-Framework/React/' }
+                        ]
+                    },
+                    {
+                        text: 'Desktop',
+                        items: [
+                            {
+                                text: 'Electron',
+                                link: '/FE-Framework/Electron/'
+                            }
+                        ]
+                    },
+                    {
+                        text: 'Mobile',
+                        items: [
+                            { text: 'Native', link: '/FE-Framework/Native/' },
+                            { text: 'H5', link: '/FE-Framework/H5/' },
+                            { text: 'Hybrid', link: '/FE-Framework/Hybrid/' },
+                            { text: 'Flutter', link: '/FE-Framework/Flutter/' }
+                        ]
+                    },
+                    {
+                        text: 'AppInner',
+                        items: [
+                            { text: 'MiniApp', link: '/FE-Framework/MiniApp/' }
+                        ]
+                    }
+                ]
+            },
+            {
+                text: 'Build',
+                items: [
+                    { text: 'Webpack', link: '/Build/Webpack/' },
+                    { text: 'Git', link: '/Build/Git/' },
+                    { text: 'Test', link: '/Build/Test/' }
+                ]
+            },
+            {
+                text: 'Tools',
+                items: [
+                    { text: 'Vscode', link: '/Tools/Vscode/' },
+                    { text: 'Vuepress', link: '/Tools/Vuepress/' }
+                ]
+            },
+            { text: 'Network', link: '/Network/' },
+            { text: 'Node', link: '/Node/' },
+            {
+                text: 'BE-Framework',
+                items: [
+                    { text: 'Express', link: '/BE-Framework/Express/' },
+                    { text: 'Koa', link: '/BE-Framework/Koa/' }
+                ]
+            },
+            { text: 'GitHub', link: 'https://wwww.baidu.com' }
+        ],
+
+        sidebar: {
+            '/Build/Webpack/': [
+                '',
+                {
+                    title: '基础入门',
+                    collapsable: false,
+                    children: ['Intro', 'Module', 'InstallAndUsage']
+                },
+                {
+                    title: '配置项',
+                    collapsable: false,
+                    children: ['Entry', 'Output']
+                },
+                {
+                    title: '项目构建实践',
+                    collapsable: false,
+                    children: ['Html']
+                }
+            ]
+        }
+    }
+}
---
+++ docs/Tools/Vuepress/README.md
@@ -0,0 +1,6 @@
+# Vuepress
+
+## 参考链接
+
+[文档：一步步搭建 VuePress 及优化](https://juejin.im/post/5c9efe596fb9a05e122c73f1)
+[视频：一步步搭建 VuePress 及优化](https://www.bilibili.com/video/av43316513/)
